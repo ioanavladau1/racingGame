a29b0ce188b83765ae178336a67b828acde2c1c0
ioana.cora <ioana.cora@casomes.ro>
2019-06-26 20:08:12 +0300

    studied polymorphism

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,86 +6,108 @@ package org.fasttrackit;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-
-        Car car = new Car(engine);
+ public static void main( String[] args )
+ {
+     Game game= new Game();
+     game.start();
 
 
 
-        car.setName("Dacia");
-        car.setColor("blue");
-        car.doorCount = 4;
-        car.setMileage(9.5);
-        car.setFluelLevel(50);
-        car.setRunning(true);
 
+//
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//
+//        Car car = new Car(engine);
+//
+//
+//
+//        car.setName("Dacia");
+//        car.setColor("blue");
+//        car.doorCount = 4;
+//        car.setMileage(9.5);
+//        car.setFluelLevel(50);
+//        car.setRunning(true);
+//
+//
+//
+//        System.out.println("Our engine...");
+//        System.out.println(car.engine.manufacturer);
+//
+//
+//        Car car1 = new Car(engine);
+//        car1.setName("Mercedes");
+//        car1.setColor("black");
+//        car1.setMaxSpeed(150.5);
+//        car1.setFluelLevel(30);
+//        car1.setRunning(false);
+//
+//        Car car3 = car;
+//
+//        System.out.println(car.getName());
+//        System.out.println(car3.getName());
+//
+//        System.out.println("Changing values...");
+//
+//        car.setName("Dacia Logan");
+//
+//        System.out.println(car.getName());
+//        System.out.println(car3.getName());
+//
+//        System.out.println(car.getMaxSpeed());
+//        System.out.println(car.engine);
+//
+//
+//        double distance = car.accelerate(60, 0.5);
+//        distance = car.accelerate(60, 0.5);
+//
+//
+//
+//        System.out.println("Distance in App.main: " + distance);
+//
+//        // demo for static variables (class variables)
+//        System.out.println("Studying static variables...");
+//
+//        car.totalCount = 1;
+//
+//        System.out.println("Total count for car: " + car.totalCount);
+//
+//        car1.totalCount = 2;
+//
+//        System.out.println("Total count from car after setting car1:" + car.totalCount);
+//                System.out.println("Total count for car1: " + car1.totalCount);
+//        System.out.println( "Total count from Vehicle class: " + Vehicle.totalCount);
+//
+//
+//        car1.accelerate(100);
+//
+//        new AutoVehicle(engine);
+//
+//        System.out.println("Today...");
+//
+//        System.out.println(car.toString());
+//
+//
+//        BatMobile batMobile = new BatMobile();
+//        batMobile.setName("Cheating vehicle");
+//        batMobile.accelerate(60,1);
+//        car.accelerate(60,1);
+//
+//     //polymorphism
+//    Vehicle mobile = new BatMobile();
+//    // method implementation invoked from object's class,
+//    // not from the class of the variable
+//    mobile.accelerate(30,2);
+//
+//    //variable type determines what methods
+//    // we can invoke for the stored object
+//    Vehicle testCar = new Car();
+//    //type casting
+//    ((Car)testCar).getDoorCount();
+//
+//    Car castedCar = (Car) testCar;
+//    castedCar.getDoorCount();
 
-
-        System.out.println("Our engine...");
-        System.out.println(car.engine.manufacturer);
-
-
-        Car car1 = new Car(engine);
-        car1.setName("Mercedes");
-        car1.setColor("black");
-        car1.setMaxSpeed(150.5);
-        car1.setFluelLevel(30);
-        car1.setRunning(false);
-
-        Car car3 = car;
-
-        System.out.println(car.getName());
-        System.out.println(car3.getName());
-
-        System.out.println("Changing values...");
-
-        car.setName("Dacia Logan");
-
-        System.out.println(car.getName());
-        System.out.println(car3.getName());
-
-        System.out.println(car.getMaxSpeed());
-        System.out.println(car.engine);
-
-
-        double distance = car.accelerate(60, 0.5);
-        distance = car.accelerate(60, 0.5);
-
-
-
-        System.out.println("Distance in App.main: " + distance);
-
-        // demo for static variables (class variables)
-        System.out.println("Studying static variables...");
-
-        car.totalCount = 1;
-
-        System.out.println("Total count for car: " + car.totalCount);
-
-        car1.totalCount = 2;
-
-        System.out.println("Total count from car after setting car1:" + car.totalCount);
-                System.out.println("Total count for car1: " + car1.totalCount);
-        System.out.println( "Total count from Vehicle class: " + Vehicle.totalCount);
-
-
-        car1.accelerate(100);
-
-        new AutoVehicle(engine);
-
-        System.out.println("Today...");
-
-        System.out.println(car.toString());
-
-
-        BatMobile batMobile = new BatMobile();
-        batMobile.setName("Cheating vehicle");
-        batMobile.accelerate(60,1);
-        car.accelerate(60,1);
 
     }
 }
--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -7,8 +7,15 @@ public class Car extends AutoVehicle {
 
     int doorCount;
 
-    public Car(Engine engine) {
-        super(engine);
+//    public Car(Engine engine) {
+//        super(engine);
+//    }
+
+    public int getDoorCount() {
+        return doorCount;
     }
 
+    public void setDoorCount(int doorCount) {
+        this.doorCount = doorCount;
+    }
 }
