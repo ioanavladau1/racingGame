04fde1500eb8c3a238a00125e13c218fc10dc9f7
ioana.cora <ioana.cora@casomes.ro>
2019-07-03 20:26:23 +0300

    studied arrays and for loops

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -5,7 +5,36 @@ public class Game {
     private Vehicle competitor1;
     private Vehicle competitor2;
 
-    public void start() {
+    private Track[] tracks = new Track[3];
 
+    public void start() {
+        initializeTracks();
+        displayTracks();
+
+
+    }
+
+    private void initializeTracks() {
+        Track city = new Track();
+        city.setName("Cluj Track");
+        city.setLeght(100);
+
+        tracks[0] = city;
+
+        Track forest = new Track();
+        forest.setName("Faget Tracks");
+        forest.setLeght(20);
+
+        tracks[1] = forest;
+    }
+
+    private void displayTracks() {
+        System.out.println("Available tracks:");
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + ". " + tracks[i].getName());
+            }
+
+        }
     }
 }
---
+++ src/main/java/org/fasttrackit/Track.java
@@ -0,0 +1,28 @@
+package org.fasttrackit;
+
+public class Track {
+
+    private String name;
+    private double leght;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLeght() {
+        return leght;
+    }
+
+    public void setLeght(double leght) {
+        this.leght = leght;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
